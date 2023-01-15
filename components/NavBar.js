6e8543a69ce0e64e1f37335a76a9e9ea44f691ef
jihye0526 <samtafe97@naver.com>
2023-01-15 13:36:41 +0900

    1.4 CSS Modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from './NavBar.module.css'
 
 export default function NavBar(){
     const router = useRouter();
@@ -7,10 +8,17 @@ export default function NavBar(){
     return (
         <nav>
             <Link href="/" legacyBehavior>
-                <a style={{ color: router.pathname === "/" ? "red" : "blue"}}>Home</a>
+                <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>  
+                {/* styles.link는 항상 적용하고 router.pathname이 일치할때 styles.active를 동작하도록 함 */}
             </Link>
             <Link href="/about" legacyBehavior>
-                <a style={{ color: router.pathname === "/about" ? "red" : "blue"}}>About</a>
+                <a className={[
+                    styles.link,
+                    router.pathname === "/about" ? styles.active : "",
+                ].join(" ")}
+            >
+                About
+            </a>
             </Link>
         </nav>
     )
