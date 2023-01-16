4d922b465f70267b4c4f2ce18ece6210f734253e
jihye0526 <samtafe97@naver.com>
2023-01-16 21:28:04 +0900

    2.2 Redirect and Rewrite

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Layout from '@/components/Layout';
-// import "../styles/globals.css" 다른파일에서는 동작을 하지 않음. _app.js에서만 동작
+import "../styles/globals.css" // 다른파일에서는 동작을 하지 않음. _app.js에서만 동작
 
 export default function App({Component, pageProps}){
     return (
